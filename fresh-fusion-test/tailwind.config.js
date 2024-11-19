caf99ae85e3e309f14f78de783df9e5fdae31de5
AE8278 <AE8278@student.jamk.fi>
2024-11-20 00:32:12 +0200

    import colors and fonts

--- fresh-fusion-test/tailwind.config.js
+++ fresh-fusion-test/tailwind.config.js
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black": "#000000",
+        "pink700": "#be185d",
+        "pink600": "#B05366",
+        "white": "#ffffff",
+      },
+      fontFamily: {
+        'jost': ['Jost', 'sans-serif'],
+        'josefin': ['Josefin Sans', 'sans-serif']
+      },
+      fontSize: {
+        'xl': ['1.25rem', { lineHeight: 'normal' }],
+        '5xl': ['3rem', { lineHeight: 'normal' }],
+      },
+      fontWeight: {
+        light: 300,
+        normal: 400,
+        medium: 500,
+      },
+    },
   },
   plugins: [],
 }
