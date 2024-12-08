8bc1bf9e6c435894ab5bbe48bde5bf0bff23760c
AE8278 <AE8278@student.jamk.fi>
2024-12-08 14:58:58 +0200

    add menuRoute to server.js

--- fresh-fusion-backend/server.js
+++ fresh-fusion-backend/server.js
@@ -1,19 +1,18 @@
-
-//server.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
+const config = require('./utils/config');
+const menuRoutes = require('./routes/menu');
+const port = config.PORT || 5000;
 
 const orderRoutes = require('./routes/orders');
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -21,9 +20,10 @@ mongoose.connect(process.env.MONGO_URI, {
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('Connected to MongoDB');
+  console.log('connected to SushiLover - MongoDB');
 });
 
+app.use('/api/menu', menuRoutes);
 app.use('/api/orders', orderRoutes);
 
 app.listen(port, () => {
