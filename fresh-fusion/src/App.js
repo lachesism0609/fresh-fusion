c3ae8dd62631e8395d4aef54327866b32ec40160
AE8278 <ae8278@student.jamk.fi>
2025-06-10 20:44:55 +0800

    change to HashRouter

--- fresh-fusion/src/App.js
+++ fresh-fusion/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
   Navigate
 } from "react-router-dom";
@@ -23,7 +23,7 @@ const ProtectedRoute = ({ children }) => {
 };
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
       element: <HomePage />,
@@ -52,9 +52,7 @@ function App() {
       path: "/address",
       element: <Address />,
     },
-  ], {
-    basename: '/sushi-lover'
-  });
+  ]);
 
   return (
     <React.StrictMode>
